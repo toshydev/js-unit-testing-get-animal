3b5782d0ee993b5ec5e55964250d0f9a61b4b710
Anton Roters <antonroters@hotmail.com>
2023-03-03 11:19:20 +0100

    finish getAnimal function

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  return animal === undefined
+    ? "I do not like animals at all!"
+    : animal === "dogs"
+    ? `I like ${animal}!`
+    : animal === "cats"
+    ? `I totally love ${animal}!`
+    : `I love ${animal}!`;
+}
